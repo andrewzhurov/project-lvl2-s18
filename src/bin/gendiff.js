28e72d459b63acbd14af0822d793af38f15c068f
Andrew <brownmoose3q@gmail.com>
2017-02-07 17:11:27 +0300

    Sane name given

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 // @flow
 import program from 'commander';
-import core from '../index';
+import gendiff from '../index';
 
 program
   .version('0.0.1')
   .arguments('<first_config> <second_config>')
   .action((f, s) => {
-    console.log(core(f, s, program.format));
+    console.log(gendiff(f, s, program.format));
   })
   .option('-f, --format [type]', 'Output format', 'json')
   .parse(process.argv);
