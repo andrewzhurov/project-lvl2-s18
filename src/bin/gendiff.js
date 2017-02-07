b961b4360d80da0896cc7f4dd931344b1d04bffc
Andrew <brownmoose3q@gmail.com>
2017-02-07 16:46:53 +0300

    .action beaten

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -10,10 +10,8 @@ program
   .version('0.0.1')
   .arguments('<first_config> <second_config>')
   .action((f, s) => {
-    firstConfig = f;
-    secondConfig = s;
+    console.log(core(f, s, program.format));
   })
   .option('-f, --format [type]', 'Output format', 'json')
   .parse(process.argv);
 
-console.log(core(firstConfig, secondConfig, program.format));
