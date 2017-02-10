39e2c89c10f378eb79c2eefe4aa44fedd3fbcc9c
Andrew <brownmoose3q@gmail.com>
2017-02-10 16:54:25 +0300

    Stuck on ast

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,93 @@
-import core from '../src/index'
+import gendiff, { gen, diffToText } from '../src/index';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(3).toBe(3);
+test('gen flat map test, no arrays', () => {
+  const f = { one: 'one', two: 'two', kangaroo: 'kangaroo' };
+  const s = { one: 'one', three: 'three', kangaroo: 'roorooroo!' };
+  // const diff = { one: { same: 'one' },
+  //                two: { removed: 'two' },
+  //                three: { added: 'three' },
+  //                kangaroo: { modified: { from: 'kangaroo', to: 'roorooroo!' } },
+  //              };
+  const diff = { status: 'same',
+                 type: 'map',
+                 entries: [{ status: 'same', key: 'one', val: { type: 'simple', raw: 'one' } },
+                           { status: 'removed', key: 'two', val: { type: 'simple', raw: 'two' } },
+                           { status: 'modified', from: { key: 'kangaroo', val: { type: 'simple', raw: 'kangaroo' } }, to: { key: 'kangaroo', val: { type: 'simple', raw: 'roorooroo!' } } },
+                           { status: 'added', key: 'three', val: { type: 'simple', raw: 'three' } }] };
+
+  expect(gen(f, s)).toEqual(diff);
 });
+
+test('gen typemodification test', () => {
+  const f = [];
+  const s = {};
+  const diff = { status: modified, from: { status: 'same', type: 'array', entries: [] }, to: { status: 'same', type: 'map', entries: [] } };
+
+  expect(gen(f, s)).toEqual(diff);
+});
+
+test('diffToText simple test', () => {
+  const diff = { num: { modified: { from: 1, to: 2 } } };
+  const text = '{\n  + num: 2\n  - num: 1\n}';
+
+  expect(diffToText(diff)).toBe(text);
+});
+
+// test('diffToText test complex', () => {
+//   const diff = { pairs: [{m: {same: 'John'},
+//                           w: {modified: {from: 'Sarah',
+//                                          to: 'Jessy'}}},
+//                          {removed: {m: 'Danny',
+//                                     w: 'Betty'}},
+//                          {added: {m: 'Bob',
+//                                   w: 'Veronica'}},
+//                         ],
+//                  single: [{added: 'Sarah'},
+//                           {removed: 'Jessy'},
+//                           {added: 'Danny'},
+//                           {added: 'Betty'},
+//                           {removed: 'Bob'},
+//                           {removed: 'Veronica'},
+//                          ],
+//                }
+//
+//   const text = '{\n\t'
+// };
+ 
+
+// fn gen [in]
+// map? in => {(keys in) (map gen (vals in))}
+// seq? in => map gen in
+// val! => return {state}
+
+// fn gen [in1 in2]
+// if map? in1
+//    if map? in2
+//       {keys : map gen vals}
+//       {removed: in1}
+//    if map? in2
+//       {added: in2}
+//
+// if seq? in1
+//    if seq? in2
+//       map gen vals
+//       {removed: in1}
+//    if seq? in2
+//       {added: in2}
+//
+// val!
+//      diff in1 in2
+ 
+//fn gen [in1 in2]
+// when (type1 !== type2) => diff in1 in2
+// 
+// case (type in1)
+//      map => {keys : map gen vals}
+//      seq => map gen vals
+//      else => 
+
+// diff [in1 in2]
+// cond
+//      (= in1 in2) {:same in1}
+
+// {key val} [val val] val
--- src/index.js
+++ src/index.js
@@ -1,6 +1,81 @@
-const fs = require('fs');
+/* eslint no-nested-ternary: "off" */
+import fs from 'fs';
+import _ from 'lodash';
 
-export default (first, second, format) => {
-  const obj = JSON.parse(fs.readFileSync(first, 'utf8'));
-  return obj;
+// tree may contain only those types, so not count as 'hardcoded'
+const type = it => typeof it !== 'object' ? 'value' :
+                     Array.isArray(it) ? 'array' :
+                     'map';
+
+
+const status = (f, s) => {
+  const ftype = type(f);
+  const stype = type(s);
+  return ftype === stype ? 'same' :
+         s === undefined ? 'removed' :
+         f === undefined ? 'added' :
+         'modified';
+};
+
+export const gen = (f, s) => {
+  const stat = status(f, s);
+  const ftype = type(f);
+  const stype = type(s);
+  return stat === 'modified' ? { stat, from: gen(f, f), to: gen(s, s) } :
+         stat === 'added' ? { stat, type: stype, val: gen(s, s) } :
+         stat === 'removed' ? { stat, type: ftype, val: gen(f, f) } :
+         stat === 'same' // < ended here
+};
+
+
+
+
+
+
+// ----------------------------- previous try below
+// NO ENTER WITH EYES OPENED
+const isDifferentType = (f, s) =>
+      (typeof f !== typeof s && typeof f !== undefined && typeof s !== undefined) ||
+      (Array.isArray(f) && !Array.isArray(s)) ||
+      (!Array.isArray(f) && Array.isArray(s));
+
+const diff = (f, s) =>
+    f === s ? { status: 'same', value: f } :
+    s === undefined ? { status: 'removed', value: f } :
+    f === undefined ? { status: 'added', value: s } :
+    { status: 'modified', from: gen(f, f), to: gen(s, s)};
+
+
+export const genold = (f, s) => {
+  if (isDifferentType(f, s)) { return diff(f, s); }
+
+  // TODO place array branch here
+  if (typeof f === 'object') {
+    const ks = _.union(Object.keys(f), Object.keys(s));
+
+    const entries = ks.map((key) => {
+      const state = gen(f[key], s[key]);
+      return { key, state };
+    });
+
+    return { type: 'map', entries };
+  }
+
+  return diff(f, s); };
+
+
+  // TODO that's a dumb realisation only to pass step2
+export const diffToText = (d) => {
+  const elmts = d.map((el) => null)
+        
+};
+// -----------------------------
+
+export default (first, second) => {
+  const fobj = JSON.parse(fs.readFileSync(first, 'utf8'));
+  const sobj = JSON.parse(fs.readFileSync(second, 'utf8'));
+
+  const astDiff = gen(fobj, sobj);
+
+  return astDiff;
 };
