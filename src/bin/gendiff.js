4631ca8194e92adc5743c8e9c15fb08ad37e2261
Andrew <brownmoose3q@gmail.com>
2017-02-12 14:48:06 +0300

    polish style

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,14 +1,13 @@
 #!/usr/bin/env node
 // @flow
 import program from 'commander';
-import util from 'util';
 import gendiff from '../index';
 
 program
   .version('0.0.1')
   .arguments('<first_config> <second_config>')
   .action((f, s) => {
-    console.log(gendiff(f, s, program.format),);
+    console.log(gendiff(f, s, program.format));
   })
   .option('-f, --format [type]', 'Output format', 'json')
   .parse(process.argv);
