2f88300eeb95396dfac6c9586e35e490b3c87284
Andrew <brownmoose3q@gmail.com>
2017-02-12 14:10:27 +0300

    Fix terminal output

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -8,7 +8,7 @@ program
   .version('0.0.1')
   .arguments('<first_config> <second_config>')
   .action((f, s) => {
-    console.log(util.inspect(gendiff(f, s, program.format), { showHidden: false, depth: null }));
+    console.log(gendiff(f, s, program.format),);
   })
   .option('-f, --format [type]', 'Output format', 'json')
   .parse(process.argv);
